a6279811d38bbaa4be461d565877be17b7b3cd10
adarsh_koru <104086266+adarshkoru0311@users.noreply.github.com>
2024-03-24 22:32:41 +0530

    Time: 5 ms (68.61%), Space: 58.2 MB (55.10%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int findDuplicate(int[] a) {
+
+        // Floyd's Cycle-Finding algorithm (without modifying array)
+        int s=0,f=0;
+        do{
+            s=a[s];
+            f=a[a[f]];
+        }while(s!=f);
+
+        s=0;
+        while(s!=f){
+            s=a[s];
+            f=a[f];
+        }
+        return s;
+        
+
+        // Modifying the array by changing the values to negative
+        /*
+        int index=0;
+        for(int i=0;i<a.length;i++)
+        {
+            index=Math.abs(a[i]);
+            if(a[index]<0)
+                return index;
+            a[index] = -a[index];
+        }
+        return -1;
+        */
+    }
+}
